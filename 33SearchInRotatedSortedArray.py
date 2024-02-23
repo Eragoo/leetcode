3e48f33be4fff31186c57f5cd7b394d0911b781a
Eugene Kukhol <eugene.k@fundomate.com>
2024-02-23 09:50:33 +0200

    add 33 partially solved

--- 33SearchInRotatedSortedArray.py
+++ 33SearchInRotatedSortedArray.py
@@ -3,42 +3,46 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        s = nums[0]
-        e_i = len(nums) - 1
-        e = nums[e_i]
-
-        if s < e or s == e:
-            return nums[0]
-
         start = 0
-        end = e_i
+        end = len(nums) - 1
 
-        def inx(i, n):
-            if i > n:
-                return i % n - 1
-            if i < 0:
-                return n + i
-            return i
-
-        while start < end:
-            mid = int(start + ((end - start) / 2))
+        while start <= end:
+            mid = (end + start) // 2
             mid_e = nums[mid]
 
-            mid_l = nums[inx(mid - 1, e_i)]
-            mid_r = nums[inx(mid + 1, e_i)]
+            if mid_e == target:
+                return mid
 
-            if mid_e < mid_l or mid_e > mid_r:
-                return min(mid_e, mid_l, mid_r)
+            # mid in left part
+            if start <= mid_e:
+                if target > mid_e:
+                    start = mid + 1
+                else:
+                    if target == nums[end]:
+                        return end
+                    if target < nums[end]:
+                        start = mid + 1
+                        end = end - 1
+                    if target > nums[end]:
+                        start = 0
+                        end = mid_e - 1
 
-            if mid_e < e and mid_e < s:
-                end = mid - 1
-            if mid_e > s and mid_e > e:
-                start = mid + 1
+                # mid in right part
+            else:
+                if target > mid_e:
+                    if target == nums[start]:
+                        return start
+                    if target > nums[start]:
+                        start = start + 1
+                        end = mid - 1
+                    else:
+                        start = mid + 1
+                        end = end
+                else:
+                    end = mid - 1
 
         return -1
 
-
 if __name__ == '__main__':
-    # print(Solution().findMin([2,3,4,5,1]))
-    print(Solution().search([2], 2))
-
+    print(Solution().search([4, 5, 6, 7, 0, 1, 2], 0))
+    # print(Solution().search([2], 2))
