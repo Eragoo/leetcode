55ff88e126b9086b5270d3d3ef478e6d436592c5
Eugene Kukhol <eugene.k@fundomate.com>
2024-02-29 11:25:05 +0200

    draft 4

--- 4MedianOfTwoSortedArrays.py
+++ 4MedianOfTwoSortedArrays.py
@@ -3,39 +3,40 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) == 0:
-            if len(nums2) % 2 == 0:
-                return nums2[(len(nums2) - 1) // 2] + nums2[((len(nums2) - 1) // 2) - 1];
-            else:
-                return nums2[(len(nums2) - 1) // 2]
+        A, B = nums1, nums2
 
-        if len(nums2) == 0:
-            if len(nums1) % 2 == 0:
-                return nums1[(len(nums1) - 1) // 2] + nums1[((len(nums1) - 1) // 2) - 1];
-            else:
-                return nums1[(len(nums1) - 1) // 2]
+        if len(A) > len(B):
+            A, B = B, A
 
-        l = 0
-        r = len(nums1) - 1
-        total = len(nums1) + len(nums2)
-        while l <= r:
-            mid = (l + r) // 2
-            mid2 = (total // 2) - (mid + 1) - 1
+        total = len(A) + len(B)
+        half = total // 2
 
-            if mid + 1 > len(nums1) - 1 and mid2 < 0:
-                return (nums1[len(nums1) - 1] + nums2[0]) / 2
+        l, r = 0, len(A) - 1
+        while True:
+            i = l + r // 2
+            j = half - i - 2
 
-            if nums1[mid] <= nums2[mid2 + 1] and nums2[mid2] <= nums1[mid + 1]:
-                if (total % 2) == 0:
-                    return (nums1[mid + 1] + nums2[mid2 + 1]) / 2
+            aL = A[i] if i >= 0 else float("-infinity")
+            bL = B[j] if i >= 0 else float("-infinity")
+
+            aR = A[i+1] if i + 1 <= len(A) - 1 else float("infinity")
+            bR = B[j+1] if i + 1 <= len(B) - 1 else float("infinity")
+
+            if aL <= bR and aR >= bL:
+                if total % 2 == 0:
+                    return (max(aL, bL) + min(aR, bR)) / 2
                 else:
-                    return min(nums1[mid + 1], nums2[mid2 + 1])
+                    return min(aR, bR)
+            elif aL > bR:
+                r = i - 1
             else:
-                l = mid + 1
+                l = i + 1
 
 
 if __name__ == '__main__':
     # print(Solution().findMedianSortedArrays([1, 2, 3, 4], [1, 2, 5]))
-    # print(Solution().findMedianSortedArrays([1, 2], [3, 4]))
+    print(Solution().findMedianSortedArrays([1, 2], [3, 4]))
     # print(Solution().findMedianSortedArrays([], [4]))
-    print(Solution().findMedianSortedArrays([1,3], [2,7]))
+    # print(Solution().findMedianSortedArrays([1,3], [2,7]))
+    # print(Solution().findMedianSortedArrays([], [2, 3]) == 2.5)
+    # print(Solution().findMedianSortedArrays([3], [-2, -1]))
