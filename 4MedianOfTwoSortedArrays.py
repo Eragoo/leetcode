e18e487a6a99e48862603a4598dc06d193ab12c2
Eugene Kukhol <eugene.k@fundomate.com>
2024-03-04 07:08:16 +0200

    fix 4

--- 4MedianOfTwoSortedArrays.py
+++ 4MedianOfTwoSortedArrays.py
@@ -13,14 +13,14 @@ class Solution:
 
         l, r = 0, len(A) - 1
         while True:
-            i = l + r // 2
+            i = (l + r) // 2
             j = half - i - 2
 
             aL = A[i] if i >= 0 else float("-infinity")
-            bL = B[j] if i >= 0 else float("-infinity")
+            bL = B[j] if j >= 0 else float("-infinity")
 
-            aR = A[i+1] if i + 1 <= len(A) - 1 else float("infinity")
-            bR = B[j+1] if i + 1 <= len(B) - 1 else float("infinity")
+            aR = A[i + 1] if i + 1 <= len(A) - 1 else float("infinity")
+            bR = B[j + 1] if j + 1 <= len(B) - 1 else float("infinity")
 
             if aL <= bR and aR >= bL:
                 if total % 2 == 0:
@@ -35,7 +35,8 @@ class Solution:
 
 if __name__ == '__main__':
     # print(Solution().findMedianSortedArrays([1, 2, 3, 4], [1, 2, 5]))
-    print(Solution().findMedianSortedArrays([1, 2], [3, 4]))
+    print(Solution().findMedianSortedArrays([1, 2, 3], [4, 5, 6]))
+    # print(Solution().findMedianSortedArrays([1, 2], [3, 4]))
     # print(Solution().findMedianSortedArrays([], [4]))
     # print(Solution().findMedianSortedArrays([1,3], [2,7]))
     # print(Solution().findMedianSortedArrays([], [2, 3]) == 2.5)
