769698d688fd26b3f19f5f7f549ba3d341e1e69e
Eugene Kukhol <eugene.kukhol@traderepublic.com>
2025-01-18 10:18:22 +0100

    add 100 same tree

---
+++ 100SameTree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q or p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    p = TreeNode(1)
+    q = TreeNode(2)
+
+    print(s.isSameTree(p, q) == False)
+
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    q = TreeNode(2)
+    print(s.isSameTree(p, q) == False)
+
+
